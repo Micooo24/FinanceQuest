8814e7216410e29b7c8d3c41ef22b1c59099c299
Justine Juliana Balla <justinejuliana.balla@tup.edu.ph>
2025-03-07 15:01:11 +0800

    community forum

--- backend/firebase_backend/firebaseconfig.py
+++
@@ -1,8 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials
-
-# Update the path to the correct location of your serviceAccountKey.json file
-
-#UPDATE NYO TONG PATH NA TO SA LOCATION NG SERVICE ACCOUNT KEY NYO
-cred = credentials.Certificate("D:/AWESOME BLOSSOM/Acads/3rdYear/2nd Sem/FinancialQuest/FinanceQuest/backend/firebase_backend/serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
